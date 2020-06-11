4ae5defa5511eec780541ffbf10286fe1b46c540
Anton Boutkam <anton@nui-boutkam.nl>
2020-06-11 12:16:58 +0200

    Trying to solve autoload issue

---
+++ src/Hi/Plugin.php
@@ -0,0 +1,25 @@
+<?php
+namespace Hi;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Plugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $oInstallationManager = $composer->getInstallationManager();
+        $oSiteInstaller = new Site\Installer($io, $composer);
+        $oInstallationManager->addInstaller($oSiteInstaller);
+
+        $oDomainInstaller = new Api\Installer($io, $composer);
+        $oInstallationManager->addInstaller($oDomainInstaller);
+
+        $oApiInstaller = new Domain\Installer($io, $composer);
+        $oInstallationManager->addInstaller($oApiInstaller);
+
+        $oApiInstaller = new Core\Installer($io, $composer);
+        $oInstallationManager->addInstaller($oApiInstaller);
+    }
+}
