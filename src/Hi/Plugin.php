b55aea8db3380674a6bbfbd5430b586bd0b0b5d7
Anton Boutkam <anton@nui-boutkam.nl>
2020-06-11 12:55:13 +0200

    Another attempt to get plugin loader to work

--- src/Hi/Plugin.php
+++ src/Hi/Plugin.php
@@ -4,22 +4,27 @@ namespace Hi;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use Hi\Installer\Site\Installer as SiteInstaller;
+use Hi\Installer\Api\Installer as ApiInstaller;
+use Hi\Installer\Domain\Installer as DomainInstaller;
+use Hi\Installer\Core\Installer as CoreInstaller;
 
 class Plugin implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
+        echo "Activating plugins" . PHP_EOL;
         $oInstallationManager = $composer->getInstallationManager();
-        $oSiteInstaller = new Site\Installer($io, $composer);
+        $oSiteInstaller = new SiteInstaller($io, $composer);
         $oInstallationManager->addInstaller($oSiteInstaller);
 
-        $oDomainInstaller = new Api\Installer($io, $composer);
+        $oDomainInstaller = new ApiInstaller($io, $composer);
         $oInstallationManager->addInstaller($oDomainInstaller);
 
-        $oApiInstaller = new Domain\Installer($io, $composer);
+        $oApiInstaller = new DomainInstaller($io, $composer);
         $oInstallationManager->addInstaller($oApiInstaller);
 
-        $oApiInstaller = new Core\Installer($io, $composer);
+        $oApiInstaller = new CoreInstaller($io, $composer);
         $oInstallationManager->addInstaller($oApiInstaller);
     }
 }
