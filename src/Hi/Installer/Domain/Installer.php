b8d071195ec9f52ce639d5d412bc11379758acb1
Anton Boutkam <anton@nui-boutkam.nl>
2020-10-02 14:30:00 +0200

    Fixed paths

--- src/Hi/Installer/Domain/Installer.php
+++ src/Hi/Installer/Domain/Installer.php
@@ -83,8 +83,8 @@ class Installer extends AbstractInstaller implements InstallerInterface
             unlink($sDestMigrationScript);
         }
 
-        $oConsole->log("Symlinking ---> {$oDirectoryStructure->getSystemDir()}/build/_skel/migrate.sh ----> $sDestMigrationScript");
-        symlink( "{$oDirectoryStructure->getSystemDir()}/build/_skel/migrate.sh", $sDestMigrationScript);
+        $oConsole->log("Symlinking ---> ../../build/_skel/migrate.sh ----> $sDestMigrationScript");
+        symlink( "../../build/_skel/migrate.sh", $sDestMigrationScript);
 
         /**
          * Create public symlink
